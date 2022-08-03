3d24ac45fed87ee80997d74a10d519377d9238e9
Mahalakshmi Durai <magi.suri@gmail.com>
2022-08-03 07:45:09 -0700

    Java implementation

--- BinaryConvert/src/queueoperation.java
+++ BinaryConvert/src/queueoperation.java
@@ -8,32 +8,11 @@ class queueoperation
       Queue<Integer> q1;
         q1 = new PriorityQueue<Integer>();
       add(q1);
-      //q1.add();
-      //q1.peek();
-      //q1.size();
-      //q1.remove();
-    //   for(int i=1;i<10;i++)
-    //     {
-    //         q1.add(i);
-    //     }
-        
-            int result=q1.peek();
-            System.out.println("First element in queue is:");
-            System.out.println(result);
-        
-            System.out.println("Length of the queue is:");
+      peek(q1);
+      size(q1);
+      remove(q1);            
+    }        
 
-            int result1=q1.size();
-            System.out.println(result1);
-            
-                for(int i=1;i<10;i++)
-        {
-           int queueitems= q1.remove();
-           System.out.println(queueitems);
-
-        }
-    }
-   
     static void add(Queue<Integer> q1)
     {
         for(int i=1;i<10;i++)
@@ -42,23 +21,23 @@ class queueoperation
         }
     }
 
-    /*
-    static void peek()
+    
+    static void peek(Queue<Integer> q1)
     {
        
             int result=q1.peek();
+            System.out.println("First element in queue is:");
             System.out.println(result);
         
     }
-    static void size()
+    static void size(Queue<Integer> q1)
     {
-        for(int i=1;i<10;i++)
-        {
-            int result1=q1.size();
-            System.oot.println(result1);
-        }
+        System.out.println("Length of the queue is:");
+        int result1=q1.size();
+        System.out.println(result1);
     }
-    static void remove()
+
+    static void remove(Queue<Integer> q1)
     {
         for(int i=0;i<10;i++)
         {
@@ -66,5 +45,5 @@ class queueoperation
            System.out.println(queueitems);
 
         }
-    }*/
+    }
 }
